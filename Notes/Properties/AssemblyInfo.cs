110f3088b9624e847576825d8e1b2d9f40aba019
Berdi2 <Benedikt.Daniel1@web.de>
2022-05-17 19:25:46 +0200

    Changed Version to 1.8 Alpha 2

--- Notes/Properties/AssemblyInfo.cs
+++ Notes/Properties/AssemblyInfo.cs
@@ -54,4 +54,4 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: nUpdateVersion("1.8a1")]
+[assembly: nUpdateVersion("1.8a2")]
